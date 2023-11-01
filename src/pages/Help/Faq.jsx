aee850e67aeb914c597ec47abbf3839148756686
user <raheemadebayorr@gmail.con>
2023-11-01 23:38:24 +0100

    created Help component with nested faq and contact pages

---
+++ src/pages/Help/Faq.jsx
@@ -0,0 +1,36 @@
+const Faq = () => {
+    return ( 
+        <>
+            <div className="faq">
+                <h3>Frequently Asked Questions</h3>
+
+                <div className="question">
+                    <p><b>Lorem ipsum dolor sit.</b></p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil accusamus distinctio 
+                        molestias ab vel in nostrum mollitia natus architecto earum? Optio sapiente facere repellendus eum?
+                    </p>
+                </div>
+                <div className="question">
+                    <p><b>Lorem ipsum dolor sit.</b></p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil accusamus distinctio 
+                        molestias ab vel in nostrum mollitia natus architecto earum? Optio sapiente facere repellendus eum?
+                    </p>
+                </div>
+                <div className="question">
+                    <p><b>Lorem ipsum dolor sit.</b></p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil accusamus distinctio 
+                        molestias ab vel in nostrum mollitia natus architecto earum? Optio sapiente facere repellendus eum?
+                    </p>
+                </div>
+                <div className="question">
+                    <p><b>Lorem ipsum dolor sit.</b></p>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil accusamus distinctio 
+                        molestias ab vel in nostrum mollitia natus architecto earum? Optio sapiente facere repellendus eum?
+                    </p>
+                </div>
+            </div>
+        </>
+     );
+}
+ 
+export default Faq;
